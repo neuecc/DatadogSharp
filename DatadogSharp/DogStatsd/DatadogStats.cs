e4c8ba5070c20cc170d576022a06284680372188
neuecc <ils@neue.cc>
2017-04-20 21:26:25 +0900

    initial push

---
+++ DatadogSharp/DogStatsd/DatadogStats.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DatadogSharp.DogStatsd
+{
+    public class DatadogStats : IDisposable
+    {
+        public static DatadogStats Default { get; private set; } = new DatadogStats();
+
+        public static void ConfigureDefault(string name, int port, string metricNamePrefix = null, string[] defaultTags = null)
+        {
+            Default.Dispose();
+            Default = new DatadogStats(name, port, metricNamePrefix, defaultTags);
+        }
+
+        bool isNull;
+        string metricNamePrefix = null;
+        string defaultTags = null;
+        IPEndPoint endPoint;
+        Socket udpSocket;
+        Encoding utf8 = Encoding.UTF8;
+
+        DatadogStats()
+        {
+            isNull = true;
+        }
+
+        public DatadogStats(string name, int port, string metricNamePrefix = null, string[] defaultTags = null)
+        {
+            this.metricNamePrefix = metricNamePrefix;
+            if (defaultTags != null)
+            {
+                this.defaultTags = string.Join(",", defaultTags);
+            }
+
+            this.udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            this.endPoint = new IPEndPoint(IPAddress.Parse(name), port);
+        }
+
+        public string WithPrefix(string name)
+        {
+            if (string.IsNullOrEmpty(metricNamePrefix)) return name;
+
+            return metricNamePrefix + "." + name;
+        }
+
+        public string WithDefaultTag(string tag)
+        {
+            if (defaultTags == null)
+            {
+                return tag;
+            }
+            else
+            {
+                if (tag == null || tag.Length == 0) return defaultTags;
+
+                return tag + "," + defaultTags;
+            }
+        }
+
+        public string WithDefaultTag(string[] tag)
+        {
+            if (defaultTags == null)
+            {
+                return string.Join(",", tag);
+            }
+            else
+            {
+                if (tag == null || tag.Length == 0) return defaultTags;
+
+                return string.Join(",", tag) + "," + defaultTags;
+            }
+        }
+
+        public void Send(string command)
+        {
+            Send(utf8.GetBytes(command));
+        }
+
+        public void Send(byte[] command)
+        {
+            udpSocket.SendTo(command, 0, command.Length, SocketFlags.None, endPoint);
+        }
+
+        public void Increment()
+        {
+
+        }
+
+        public void Decrement() { }
+        public void Counter() { }
+
+        public void Gauge(string metricName, long value, double sampleRate = 1.0, string[] tags = null)
+        {
+            if (isNull) return;
+            var command = DogStatsDFormatter.Gauge(metricName, value, sampleRate, WithDefaultTag(tags));
+            Send(command);
+        }
+
+        public void Gauge(string metricName, long value, double sampleRate = 1.0, string tags = null)
+        {
+            if (isNull) return;
+            var command = DogStatsDFormatter.Gauge(metricName, value, sampleRate, WithDefaultTag(tags));
+            Send(command);
+        }
+
+        public void Histogram() { }
+        public void Set() { }
+        public void Time() { }
+
+        public void Dispose()
+        {
+            if (isNull) return;
+            this.udpSocket.Dispose();
+            isNull = true;
+        }
+    }
+}
---
+++ DatadogSharp/DogStatsd/DogStatsDFormatter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Globalization;
+
+namespace DatadogSharp.DogStatsd
+{
+    /// <summary>
+    /// http://docs.datadoghq.com/guides/dogstatsd/#datagram-format
+    /// </summary>
+    public static class DogStatsDFormatter
+    {
+        static readonly IFormatProvider InvaliantCultrue = CultureInfo.InvariantCulture;
+
+        // metric.name:value|type|@sample_rate|#tag1:value,tag2
+
+        // metric.name should be a String with no colons, bars or @ characters and fit our naming policy.
+        // value should be a number
+        // type should be c for Counter, g for Gauge, h for Histogram, ms for Timer or s for Set.
+        // sample rate is optional and should be a float between 0 and 1 inclusive.
+        // tags are optional, and should be a comma seperated list of tags.Colons are used for key value tags.Note that the key device is reserved, tags like “device:xyc” will be dropped by Datadog.
+
+        static string Build(string metricName, string value, string type, double sampleRate, string tags)
+        {
+            if (sampleRate == 1.0)
+            {
+                if (tags == null || tags.Length == 0)
+                {
+                    return metricName + ":" + value + "|" + type;
+                }
+                else
+                {
+                    return metricName + ":" + value + "|" + type + "|#" + tags;
+                }
+            }
+            else
+            {
+                if (tags == null || tags.Length == 0)
+                {
+                    return metricName + ":" + value + "|" + type + "|@" + sampleRate.ToString(InvaliantCultrue);
+                }
+                else
+                {
+                    return metricName + ":" + value + "|" + type + "|@" + sampleRate.ToString(InvaliantCultrue) + "|#" + tags;
+                }
+            }
+        }
+
+        public static string Gauge(string metricName, long value, double sampleRate, string tags)
+        {
+            return Build(metricName, value.ToString(InvaliantCultrue), "g", sampleRate, tags);
+        }
+
+        public static string Gauge(string metricName, double value, double sampleRate, string tags)
+        {
+            return Build(metricName, value.ToString(InvaliantCultrue), "g", sampleRate, tags);
+        }
+
+
+        //public static string Counters(string metricName, string value)
+        //{
+        //    return metricName + ":" + value + "|" + "g";
+        //}
+        //public static string Gauge(string metricName, string value)
+        //{
+        //    return metricName + ":" + value + "|" + "g";
+        //}
+        //public static string Timers(string metricName, string value)
+        //{
+        //    return metricName + ":" + value + "|" + "g";
+        //}
+        //public static string Gauge(string metricName, string value)
+        //{
+        //    return metricName + ":" + value + "|" + "g";
+        //}
+        //public static string Gauge(string metricName, string value)
+        //{
+        //    return metricName + ":" + value + "|" + "g";
+        //}
+    }
+}
